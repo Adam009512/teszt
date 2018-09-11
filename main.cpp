3d765fc86a1c62dac8bb4610eff791c2592ef44c
adam_95 <adam951228@gmail.com>
2018-09-11 21:05:29 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a,b,sum;
+	cout << "Ener any two numbers";
+	cin>>a>>b;
+	sum = a+b;
+	cout<< "sum is = "<< sum;
+	return 0;
+}
+
